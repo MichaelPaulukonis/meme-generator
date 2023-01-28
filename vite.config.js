05142b6aceff8a8b605977f569b1668203dd1691
Michael Paulukonis <michael.paulukonis@gmail.com>
2023-01-28 10:09:16 -0500

    Moved base config outside of resolve oops

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,6 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-  base: process.env.DEPLOY_ENV === "GH_PAGES" ? "/meme-generator/" : "",
   },
+  base: process.env.DEPLOY_ENV === "GH_PAGES" ? "/meme-generator/" : "",
 })
